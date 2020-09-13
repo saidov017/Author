377928a4cba46c522369862e11c9157e69225362
saidov017 <58263437+saidov017@users.noreply.github.com>
2020-09-13 19:32:55 +0300

    Update Main.java

--- src/Author/Main.java
+++ src/Author/Main.java
@@ -3,11 +3,10 @@ package Author;
 public class Main {
 
     public static void main(String[] args) {
-        Author author = new Author("Александр Костылев", "Dolk@gmail.com", 'м');
+        Author author = new Author("Александр Костылев", "Sasha@gmail.com", 'м');
         System.out.println(author);
 
-        author.setEmail("newSasha@gmail.com");
-        System.out.println(author);
+      
     }
 
 }
